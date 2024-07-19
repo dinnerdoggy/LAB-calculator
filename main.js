37bd62f6a8c013e2433d1d2109dcca34338859aa
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2024-07-19 11:16:49 -0500

    all number buttons functioning

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ const btnEquals = document.querySelector(".btnE");
 const btn0 = document.querySelector(".btn0");
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
+const btn3 = document.querySelector(".btn3");
+const btn4 = document.querySelector(".btn4");
+const btn5 = document.querySelector(".btn5");
+const btn6 = document.querySelector(".btn6");
+const btn7 = document.querySelector(".btn7");
+const btn8 = document.querySelector(".btn8");
+const btn9 = document.querySelector(".btn9");
 
 //button actions
 btnEquals.addEventListener("click", function() {
@@ -29,4 +36,25 @@ btn1.addEventListener("click", function() {
 })
 btn2.addEventListener("click", function() {
   resultField.innerHTML += 2;
+})
+btn3.addEventListener("click", function() {
+  resultField.innerHTML += 3;
+})
+btn4.addEventListener("click", function() {
+  resultField.innerHTML += 4;
+})
+btn5.addEventListener("click", function() {
+  resultField.innerHTML += 5;
+})
+btn6.addEventListener("click", function() {
+  resultField.innerHTML += 6;
+})
+btn7.addEventListener("click", function() {
+  resultField.innerHTML += 7;
+})
+btn8.addEventListener("click", function() {
+  resultField.innerHTML += 8;
+})
+btn9.addEventListener("click", function() {
+  resultField.innerHTML += 9;
 })
