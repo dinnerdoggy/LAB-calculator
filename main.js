158e7aff1b9da736c795c7be764fe69cbf0b35b3
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2024-07-19 11:11:13 -0500

    equals button working to solve equations

--- main.js
+++ main.js
@@ -2,14 +2,24 @@
 const resultField = document.querySelector("#result")
 
 //button connections
-const btnC = document.querySelector(".btnC")
+const btnC = document.querySelector(".btnC");
+const btnPlus = document.querySelector(".btnPlus");
+const btnEquals = document.querySelector(".btnE");
 const btn0 = document.querySelector(".btn0");
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
 
 //button actions
+btnEquals.addEventListener("click", function() {
+  let equation = result.innerHTML;
+  let answer = eval(equation);
+  resultField.innerHTML = answer;
+})
 btnC.addEventListener("click", function() {
   resultField.innerHTML = null;
+})
+btnPlus.addEventListener("click", function() {
+  resultField.innerHTML += " + ";
 })
 btn0.addEventListener("click", function() {
   resultField.innerHTML += 0;
