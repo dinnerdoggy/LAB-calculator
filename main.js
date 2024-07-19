de415e3220d08a494fb0bbbe7c11f374d8964f3b
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2024-07-19 10:19:08 -0500

    connected some buttons and made them inject or erase characters into result field

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("working")
+//Result field connection
+const resultField = document.querySelector("#result")
+
+//button connections
+const btnC = document.querySelector(".btnC")
+const btn0 = document.querySelector(".btn0");
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+
+//button actions
+btnC.addEventListener("click", function() {
+  resultField.innerHTML = 0;
+})
+btn0.addEventListener("click", function() {
+  resultField.innerHTML += 0;
+})
+btn1.addEventListener("click", function() {
+  resultField.innerHTML += 1;
+})
+btn2.addEventListener("click", function() {
+  resultField.innerHTML += 2;
+})
